ea730b35bf97ae5ba53974cd6798dc68bbff724a
Kevin Chen <k39chen@gmail.com>
2014-08-22 17:55:12 -0400

    Implmented core randomizer logic and visual components

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,48 @@
 Template.app.rendered = function() {
 
+	// hide the buttons by default
+	$(".button").hide();
 
-    console.log( "here i am" );
+	// initialize the tag widget
+    $("#tags").tagit({
+    	placeholderText: "Start typing your option here!",
+    	availableTags: ["app","something","somethin gelse"],
+    	caseSensitive: false,
+    	allowSpaces: true,
+    	tabIndex: 0,
+    	animate: false,
+    	afterTagAdded: function() {
+    		var buttons = $(".button");
 
+    		// see if we should show the randomize button
+    		if (buttons.is(":hidden")) {
+    			buttons.show().css({opacity:0}).stop().animate({opacity:1},300);
+    		}
+    	},
+    	afterTagRemoved: function(e) {
+    		var $tagit = $(e.target),
+    			buttons = $(".button");
+    		if ($tagit.tagit("assignedTags").length === 0) {
+    			buttons.css({opacity:1}).stop().animate({opacity:0},300,function(){$(this).hide();});
+    		}
+    	}
+    });
 };
+Template.app.events({
+	"mouseover .button": function(e) {
+		var $el = $(e.target);
+		$el.addClass("hover");
+	},
+	"mouseout .button": function(e) {
+		var $el = $(e.target);
+		$el.removeClass("hover");
+	},
+	"click #randomize-button": function(e) {
+		var $el = $(e.target);
+		// ...
+	},
+	"click #clear-button": function(e) {
+		var $el = $(e.target);
+		$("#tags").tagit("removeAll");
+	}
+});
