fd1299a3fbb37a28295b6ab5780aba811e935fef
Kevin Chen <k39chen@gmail.com>
2014-08-23 19:44:22 -0400

    Added spinner for tagit widget to complete loading

--- client/js/app.js
+++ client/js/app.js
@@ -3,36 +3,42 @@ Template.app.rendered = function() {
 	// hide the buttons by default
 	$(".button").hide();
 	$("#result").hide();
+	$("#spinner").show().css({opacity:0}).stop().animate({opacity:1},300);
 
 	Meteor.call("getTags", function(err,data){
 		var prioritizedData = data.sort(function(a,b){return a-b;});
 		var availableTags = $.map(prioritizedData,function(item){
 			return item.label;
 		});
-		// initialize the tag widget
-	    $("#tags").tagit({
-	    	placeholderText: "Start typing your option here!",
-	    	availableTags: availableTags,
-	    	caseSensitive: false,
-	    	allowSpaces: true,
-	    	tabIndex: 0,
-	    	animate: false,
-	    	afterTagAdded: function() {
-	    		var buttons = $(".button");
 
-	    		// see if we should show the randomize button
-	    		if (buttons.is(":hidden")) {
-	    			buttons.show().css({opacity:0}).stop().animate({opacity:1},300);
-	    		}
-	    	},
-	    	afterTagRemoved: function(e) {
-	    		var $tagit = $(e.target),
-	    			buttons = $(".button");
-	    		if ($tagit.tagit("assignedTags").length === 0) {
-	    			buttons.css({opacity:1}).stop().animate({opacity:0},300,function(){$(this).hide();});
-	    		}
-	    	}
-	    });
+		$("#spinner").css({opacity:1}).stop().animate({opacity:0},300,function(){
+			$(this).hide();
+
+			// initialize the tag widget
+		    $("#tags").tagit({
+		    	placeholderText: "Start typing your option here!",
+		    	availableTags: availableTags,
+		    	caseSensitive: false,
+		    	allowSpaces: true,
+		    	tabIndex: 0,
+		    	animate: false,
+		    	afterTagAdded: function() {
+		    		var buttons = $(".button");
+
+		    		// see if we should show the randomize button
+		    		if (buttons.is(":hidden")) {
+		    			buttons.show().css({opacity:0}).stop().animate({opacity:1},300);
+		    		}
+		    	},
+		    	afterTagRemoved: function(e) {
+		    		var $tagit = $(e.target),
+		    			buttons = $(".button");
+		    		if ($tagit.tagit("assignedTags").length === 0) {
+		    			buttons.css({opacity:1}).stop().animate({opacity:0},300,function(){$(this).hide();});
+		    		}
+		    	}
+		    });
+		});
 	});
 };
 Template.app.events({
