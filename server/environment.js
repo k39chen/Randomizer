4b4e48b47202a4c8db2cbdfce636bf9548d24640
Kevin Chen <k39chen@gmail.com>
2014-08-23 19:38:28 -0400

    Applied looser restrictions on autocomplete features and tag db intro

--- server/environment.js
+++ server/environment.js
@@ -17,14 +17,13 @@ Meteor.methods({
 		for (var i=0; i<tags.length; i++) {
 			// see if we increment the counter on an existing tag
 			// or to create a new tag with a fresh counter
-			var tag = Tags.findOne({label: tags[i]});
+			var tag = Tags.findOne({label: (tags[i]+"").toLowerCase().trim()});
 
 			if (!tag) {
+				label = toTitleCase(tags[i]+"").trim();
+
 				// create a new one
-				Tags.insert({
-					label: tags[i],
-					count: 0
-				});
+				Tags.insert({label:label, count:0});
 			} else {
 				// increment its counter
 				Tags.update({_id:tag._id}, {$inc: {count: 1}});
@@ -41,3 +40,13 @@ Meteor.methods({
 		return Tags.find().fetch();
 	}
 });
+/**
+ * Returns a title-cased string.
+ *
+ * @method toTitleCase
+ * @param str {String} The string to convert to title case.
+ * @return {String} The title-cased string.
+ */
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
